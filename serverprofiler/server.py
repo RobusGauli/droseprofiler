18cc114554c8efa6f8ec255a4f56c5afec1610c7
Robus <robusgauli@gmail.com>
2017-09-10 18:06:56 +0545

    asd

---
+++ serverprofiler/server.py
@@ -0,0 +1,134 @@
+import collections
+import itertools
+import json
+import asyncio
+
+import websockets
+from sanic import Sanic
+from sanic_cors import CORS
+from sanic.response import text, json as jsonify 
+
+
+
+
+def get_header_val(headers):
+    '''returns header dict'''
+    return dict(headers)
+
+class Master:
+
+    def __init__(self):
+        self.slave_registry = {}
+
+        self.http_server = Sanic(__name__)
+        CORS(self.http_server)
+        self.create_http_app()
+
+    def create_http_app(self):
+        
+        async def snapshot(_):
+            return jsonify({'nodes': [{'cpu': self.slave_registry[key]['cpu'], 'name': key} for key in self.slave_registry.keys() 
+                                        if self.slave_registry[key]['ws'].state == 1]})
+
+        self.http_server.route('/nodes')(snapshot)
+
+    
+    async def handler(self, websocket: websockets.WebSocketClientProtocol, _: str):
+
+        print('Got connection from the client', websocket)
+
+        #check to see if it is the slave or client
+        mode = get_header_val(websocket.request_headers._headers)
+        _type_of_client = mode.get('mode')
+        _id = mode.get('id')
+        if _type_of_client == 'slave' and _id:
+            #register as the slave server
+            
+            self.slave_registry[_id] = {}
+            self.slave_registry[_id]['ws'] = websocket
+            self.slave_registry[_id]['cpu'] = mode.get('cpu') 
+            print(self.slave_registry)
+            while True:
+                await asyncio.sleep(0)
+        #since this is the client that i coming,
+        #we create a new instance of the of the connection that involves three major parameter
+        client_cluster = ClientCluster(
+            client_ws=websocket, 
+            slave_registry=self.slave_registry,
+        
+        )
+
+        websocket.loop.create_task(client_cluster.manage_production())
+        websocket.loop.create_task(client_cluster.manage_consumption())
+        while True:
+            await asyncio.sleep(0)
+            #it simply persist the connecrion and yield the control back to the event loop
+
+    def run(self):
+        _master_server = websockets.serve(
+            self.handler,
+            'localhost',
+            5000
+        )
+
+        sanic_server = self.http_server.create_server(
+            '0.0.0.0',
+            port=8000
+        )
+        sanic_task = asyncio.ensure_future(sanic_server)
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.gather(_master_server, sanic_task))
+        loop.run_forever()
+
+
+class ClientCluster:
+    '''Client cluster that represents the connected client and the registered slaves.'''
+
+    def __init__(self, client_ws, slave_registry):
+        self.client_ws = client_ws
+        self.slave_registry = slave_registry
+        self.current_slave_ws = None
+        
+    
+    async def manage_consumption(self):
+        while True:
+            try:
+                msg = await self.client_ws.recv()
+                request = json.loads(msg)
+                if not request.get('destination'):
+                    continue
+                if not self.slave_registry.get(request['destination']):
+                    continue
+                #get the desitnation slave socket
+                self.current_slave_ws = self.slave_registry[request['destination']]['ws']
+                #send the json patload to the current selected slave
+                await self.current_slave_ws.send(msg)
+                #get the response from the slave
+                response_from_current_slave = await self.current_slave_ws.recv()
+                #send back the response to the client
+                await self.client_ws.send(response_from_current_slave)
+
+            except websockets.exceptions.ConnectionClosed:
+                print('Connection closed by the client')
+                print(request)
+                self.slave_registry.pop(request['destination'])
+                print(self.slave_registry)
+                break
+
+    
+    async def manage_production(self):
+        while True:
+            try:
+                
+                await asyncio.sleep(2)
+            except websockets.exceptions.ConnectionClosed:
+                print('Connection closed by the client')
+                break
+
+
+if __name__ == '__main__':
+    m = Master()
+    m.run()
+    
+
---
+++ serverprofiler/slave.py
@@ -0,0 +1,124 @@
+import websockets
+import asyncio
+import argparse
+import platform
+
+from serverprofiler.node import Node
+
+
+class Slave:
+    '''This represents the slave node in the monitoring cluster
+        Main purpose -> register itself to the master server as the slave node
+        -> recv the command from client, connect directly to the client socket
+        -> master acts as the bridge between the client and this node
+    '''
+    DEFAULT_MASTER_HOST = 'localhost'
+    DEFAULT_MASTER_PORT = 5000
+    MODE_HEADERS = [('mode', 'slave'), ('id', 'slave_one')]
+    
+    @classmethod
+    def from_cli(cls):
+        config = cls.load_from_cli()
+        self = cls(config=config)
+        return self
+
+       
+
+
+    def prepare_mode_headers(self):
+        return [('mode', 'slave'), ('id', self.slave_id), ('cpu', platform.platform())]
+    
+    @classmethod
+    def load_from_cli(cls):
+        config = {}
+        for key, val in vars(cls._get_args()).items():
+            if val:
+                key = 'MASTER_%s' % key.upper()
+                config[key] = val
+        return config
+
+    
+    @classmethod
+    def _get_args(cls):
+        parser = argparse.ArgumentParser(
+            description='Slave Node for server profiler'
+        )
+        parser.add_argument(
+            '-host',
+            action='store',
+            type=str,
+            default=None,
+            dest='host',
+            help='Host to regsiter to.'
+        )
+
+        parser.add_argument(
+            '-p', '--port',
+            action='store',
+            type=int,
+            default=None,
+            dest='port',
+            help='Master port number'
+        )
+        parser.add_argument(
+            '-id',
+            action='store',
+            type=str,
+            dest='id',
+            help='Unique slave id',
+            required=True
+        )
+        
+        return parser.parse_args()
+
+        
+    def __init__(self, config=None):
+        self.node = Node()
+        self.config = config
+        self.ws = None
+        
+        self.master_host = config.get('MASTER_HOST') or self.DEFAULT_MASTER_HOST
+        self.master_port = config.get('MASTER_PORT') or self.DEFAULT_MASTER_PORT
+        self.slave_id = config.get('MASTER_ID') or 'Unknown'
+        self.headers = self.prepare_mode_headers()
+    
+    
+
+    async def handler(self):
+        async with websockets.connect('ws://%s:%s/' % (self.DEFAULT_MASTER_HOST, self.DEFAULT_MASTER_PORT),
+            extra_headers=self.headers) as ws:
+            self.ws = ws
+            await self._manage_client_session()
+            while True:
+                #just to keep the connection alive and yield the connection 
+                await asyncio.sleep(0)
+    
+    async def _manage_client_session(self):
+        self.ws.loop.create_task(self._manage_client_consumption())
+        self.ws.loop.create_task(self._manage_client_production())
+
+    async def _manage_client_consumption(self):
+        while True:
+            received = await self.ws.recv()
+            print(received)
+            await self.ws.send(self.slave_id + self.node.get_info())
+
+
+    async def _manage_client_production(self):
+        while True:
+            
+            await asyncio.sleep(0)
+    
+    
+    def run(self):
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.handler())
+
+    
+
+
+if __name__ == '__main__':
+    s = Slave.from_cli()
+    s.run()
+
