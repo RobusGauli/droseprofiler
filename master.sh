e9d8e3f787f3fbf0c1ddfb005933e6df5748f22f
Robus <robusgauli@gmail.com>
2017-09-14 12:49:58 +0545

    done

--- master.sh
+++ master.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-home/drose/env/bin/python home/drose/droseprofiler -yfp home/drose/droseprofiler/master.yaml
+/home/drose/env/bin/python /home/drose/droseprofiler -yfp /home/drose/droseprofiler/master.yaml
--- slave.sh
+++ slave.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-home/drose/env/bin/python home/drose/droseprofiler -s -yfp home/drose/droseprofiler/slave.yaml
+/home/drose/env/bin/python /home/drose/droseprofiler -s -yfp /home/drose/droseprofiler/slave.yaml
