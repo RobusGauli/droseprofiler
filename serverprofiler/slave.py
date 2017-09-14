aff2c37aebc5d9fce407c13f92e7f91bd4c61f0b
Robus <robusgauli@gmail.com>
2017-09-14 07:49:21 +0545

    action dispatcher added

--- serverprofiler/slave.py
+++ serverprofiler/slave.py
@@ -2,6 +2,8 @@ import websockets
 import asyncio
 import argparse
 import platform
+import json
+import functools
 
 from serverprofiler.node import Node
 
@@ -75,19 +77,26 @@ class Slave:
     def __init__(self, config=None):
         self.node = Node()
         self.config = config or self.load_from_cli()
+        #connection to the master node
         self.ws = None
         
         self.master_host = self.config.get('MASTER_HOST') or self.DEFAULT_MASTER_HOST
         self.master_port = self.config.get('MASTER_PORT') or self.DEFAULT_MASTER_PORT
         self.slave_id = self.config.get('SLAVE_ID') or 'Unknown'
         self.headers = self.prepare_mode_headers()
+        self.action_dispatcher = None
+        # self.register = {}
+        # self.action_manager()
+        # print(self.register)
+        
     
     
 
     async def handler(self):
         async with websockets.connect('ws://%s:%s/' % (self.master_host, self.master_port),
             extra_headers=self.headers) as ws:
-            
+            self.action_dispatcher = ActionDispatcher(ws)
+
             consumer_task = asyncio.ensure_future(self._manage_client_consumption(ws))
             producer_task = asyncio.ensure_future(self._manage_client_production(ws))
             done, pending = await asyncio.wait(
@@ -109,11 +118,16 @@ class Slave:
         
     async def _manage_client_consumption(self, ws):
         while True:
-            received = await ws.recv()
-            print(received)
             
-            await ws.send(self.node.get_info())
-
+            received = await ws.recv()
+            try:
+                r = json.loads(received)
+            except json.JSONDecodeError:
+                continue
+            await self.action_dispatcher.dispatch(r['route'])
+            # if route:
+            #     print(self.register[route])
+            #     await self.register[route](ws)
 
     async def _manage_client_production(self, ws):
         while True:
@@ -121,7 +135,23 @@ class Slave:
             #making it to 0 will get the cpu consumption to 100%
             await asyncio.sleep(2)
     
+    # def action_manager(self):
+
+    #     @self.route('/info')
+    #     async def get_info(ws):
+    #         await ws.send('ho from theinner function')
+        
+        
+        
     
+
+    # def route(self, route):
+        
+    #     def wrapper(func):
+    #         self.register[route] = func   
+    #     return wrapper         
+
+
     def run(self):
         
         loop = asyncio.get_event_loop()
@@ -131,6 +161,33 @@ class Slave:
     
 
 
+class ActionDispatcher:
+
+    def __init__(self, master_ws):
+        self.master_ws = master_ws
+        self.register = {}
+        self._initialize_routes_func()
+        
+
+    
+    def route(self, path):
+        def _wrapper(func):
+            self.register[path] = func
+        return _wrapper
+
+    
+    def _initialize_routes_func(self):
+        @self.route('/info')    
+        async def get_cpu_info():
+            await self.master_ws.send('hi form the action manager')
+        
+        
+    
+    async def dispatch(self, route):
+        #given the dispatch command i shoulde be abpe to perform the action
+        _func = self.register.get(route)
+        if _func:
+            await _func()
 if __name__ == '__main__':
     s = Slave.from_cli()
     s.run()
